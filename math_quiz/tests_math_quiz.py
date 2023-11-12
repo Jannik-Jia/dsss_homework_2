7da5c3f51f73ca0358546549c253fa3a0a177282
Jannik_Jia <gylz1004@gmail.com>
2023-11-12 15:36:00 +0100

    updated tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,36 @@
 import unittest
-from math_quiz import function_A, function_B, calculate
+from math_quiz import generate_random_number, choose_random_operator, calculate
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_random_number(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
+        min_value = 1
+        max_value = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+            rand_num = generate_random_number(min_value, max_value)
+            self.assertTrue(min_value <= rand_num <= max_value)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_choose_random_operator(self):
+        valid_operatiors = {'+', '-', '*', '/'}
+        for _ in range(1000): # Test a large number of times
+            operator = choose_random_operator()
+            self.assertIn(operator, valid_operatiors)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_calculate(self):
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (7, 2, '-', '7 - 2', 5),
+            (4, 3, '*', '4 * 3', 12),
+            (8, 2, '/', '8 / 2', 4)
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for operand1, operand2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = calculate(operand1, operand2, operator)
+            self.assertEqual(problem, expected_problem)
+            self.assertEqual(answer, expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
